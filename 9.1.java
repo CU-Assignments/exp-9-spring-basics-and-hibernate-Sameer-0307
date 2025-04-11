daaaae9825d42229c34f72eed2e5fa0f83d1d043
Sameer-0307 <gautamsameer710@gmail.com>
2025-04-11 11:49:44 +0530

    9.1.java

--- 9.1.java
+++ 9.1.java
@@ -15,3 +15,38 @@ public class Course {
         return duration;
     }
 }
+
+public class Student {
+    private String name;
+    private Course course;
+
+    public Student(String name, Course course) {
+        this.name = name;
+        this.course = course;
+    }
+
+    public void displayInfo() {
+        System.out.println("Student Name: " + name);
+        System.out.println("Enrolled Course: " + course.getCourseName());
+        System.out.println("Course Duration: " + course.getDuration() + " weeks");
+    }
+}
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    public Course course() {
+        return new Course("Spring Framework", 6);
+    }
+
+    @Bean
+    public Student student() {
+        return new Student("Sameer", course());
+    }
+}
+
